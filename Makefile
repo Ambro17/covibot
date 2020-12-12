8e08fc62549b35b0b3cda62aaccdc0c29e46699c
Nahuel Ambrosini <nambrosini@onapsis.com>
2020-12-12 20:45:00 -0300

    🔧 Adjust IAM permissions and add logging to stdout

---
+++ Makefile
@@ -0,0 +1,8 @@
+auth:
+	aws-google-auth -u ${USER}@onapsis.com -I C019my1yt -S 837142051107 --ask-role -d 3600
+
+do:
+	cd covibot && chalice deploy && cd -
+
+undo:
+	cd covibot && chalice delete && cd -
--- covibot/app.py
+++ covibot/app.py
@@ -1,11 +1,11 @@
+import logging
 import os
 
 from chalice import Chalice
 from chalicelib.bus import SQSBus
 from chalicelib.events import StartVMs
 
-# There's an implicit lambda created with the name
-# `api_handler` that receives all api gateway traffic
+# There's only one lambda created for all @app.route's
 # See: https://aws.github.io/chalice/topics/configfile.html#lambda-specific-configuration
 app = Chalice(app_name="covibot-api")
 bus = SQSBus(os.getenv('SQS_URL', 'MissingSQSURLEnvVar'))
@@ -20,13 +20,26 @@ def home():
 def task():
     # Launch sqs task
     args = app.current_request.json_body
-    event = StartVMs(vms=args.get('vms', {}), user_id=args.get('user_ids', [-1]))
-    bus.send(event)
+    print(f"Received POST with args: {args}")
+    if not args:
+        return {'success': False, 'error': 'POST must have application/json as content type'}
+
+    event = StartVMs(
+        vms=args.get('vms', {}),
+        user_id=args.get('user_ids', [-1])
+    )
+
+    try:
+        bus.send(event)
+    except Exception as e:
+        print(repr(e))
+        return {'exception': repr(e), 'success': False}
+
     return {"success": True}
 
 
 @app.on_sqs_message(queue=os.getenv('SQS_QUEUE_NAME'), name='start_callback')
 def execute_task(event):
-    app.log.debug("Event %r", event.to_dict())
+    print("Event %r", event.to_dict())
     for record in event:
-        app.log.info("Message body: %s", record.body)
+        print("Message body: %s" % record.body)
