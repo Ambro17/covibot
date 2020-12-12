0ea3ff46479d5c3eba1c3626fbdb849f0e227441
Nahuel Ambrosini <nambrosini@onapsis.com>
2020-12-12 11:24:45 -0300

    ✨ Track terraform json artifact

--- covibot/app.py
+++ covibot/app.py
@@ -3,7 +3,7 @@ from chalice import Chalice
 # There's an implicit lambda created with the name
 # `api_handler` that receives all api gateway traffic
 # See: https://aws.github.io/chalice/topics/configfile.html#lambda-specific-configuration
-app = Chalice(app_name="helloworld")
+app = Chalice(app_name="covibot-api")
 
 
 @app.route("/", name='hello_world_lambda')
@@ -11,6 +11,6 @@ def index():
     return {"hello": "world"}
 
 
-@app.route("/test", name='test_lambda')
+@app.route("/health", name='test_lambda')
 def index():
     return {"success": True}
