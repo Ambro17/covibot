79b15af14dd446b1943fd32a918d049bcf148d87
Nahuel Ambrosini <nambrosini@onapsis.com>
2020-12-15 12:12:37 -0300

    🎨 Beautify script

--- Makefile
+++ Makefile
@@ -1,18 +1,17 @@
 auth:
-	aws-google-auth -u ${USER}@onapsis.com -I C019my1yt -S 837142051107 --ask-role --duration 3600 --profile 'default'
+	aws-google-auth -u ${USER}@onapsis.com -I C019my1yt -S 837142051107 --ask-role --duration 3600 --profile 'default' \
+	--save-failure-html
 
 install:
 	pip install -r requirements.txt
 
 do:
 	cd covibot && \
-	# Replace secrets but save the unreplaced file as backup to avoid \
-	# Leaking secrets into source control \
+	# Replace secrets to deploy them in aws ready to be used \
 	python replace_secrets.py && \
 	chalice deploy && \
-	# Restore unreplaced file \
-	cp .chalice/config.json.bak .chalice/config.json && \
-	rm .chalice/config.json.bak && \
+	# Replace secrets with dummy values to avoid leaking secrets into source control \
+	python replace_secrets.py unreplace && \
 	cd -
 
 undo:
--- covibot/replace_secrets.py
+++ covibot/replace_secrets.py
@@ -1,23 +1,54 @@
 import json
 import os
+import sys
 from pathlib import Path
 
-config_path = Path(__name__).parent.absolute() / '.chalice' / 'config.json'
 
-with open(config_path, 'r+') as f:
-    config = json.load(f)
+chalice_config_path = Path(__name__).parent.absolute() / '.chalice'
+config_path = chalice_config_path / 'config.json'
 
-with open(f'{config_path}.bak', 'w+') as f:
-    json.dump(config, f, indent=4, ensure_ascii=False)
 
-# Replace env vars
-token = os.environ['SLACK_BOT_TOKEN']
-secret = os.environ['SLACK_SIGNING_SECRET']
-testing = os.environ['TESTING']
+def replace():
+    config = read_config()
+    dump_env_vars(config)
+    replace_config(config)
 
-config['stages']['dev']['environment_variables']['SLACK_BOT_TOKEN'] = token
-config['stages']['dev']['environment_variables']['SLACK_SIGNING_SECRET'] = secret
-config['stages']['dev']['environment_variables']['TESTING'] = testing
 
-with open(Path('.chalice/config.json'), 'r+') as f:
-    config = json.dump(config, f, indent=4, ensure_ascii=False)
+def read_config():
+    with open(config_path, 'r+') as f:
+        config = json.load(f)
+
+    return config
+
+
+def dump_env_vars(config):
+    # Replace env vars
+    token = os.environ['SLACK_BOT_TOKEN']
+    secret = os.environ['SLACK_SIGNING_SECRET']
+    testing = os.environ['TESTING']
+    config['stages']['dev']['environment_variables']['SLACK_BOT_TOKEN'] = token
+    config['stages']['dev']['environment_variables']['SLACK_SIGNING_SECRET'] = secret
+    config['stages']['dev']['environment_variables']['TESTING'] = testing
+
+    return config
+
+
+def replace_config(config):
+    with open(Path('.chalice/config.json'), 'w+') as f:
+        json.dump(config, f, indent=4, ensure_ascii=False)
+
+
+def unreplace():
+    config = read_config()
+    config['stages']['dev']['environment_variables']['SLACK_BOT_TOKEN'] = '$SBT'
+    config['stages']['dev']['environment_variables']['SLACK_SIGNING_SECRET'] = '$SSS'
+    config['stages']['dev']['environment_variables']['TESTING'] = '$TESTING'
+    replace_config(config)
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if not args:
+        replace()
+    else:
+        unreplace()
