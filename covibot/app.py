e5b5a5517c8d1126c478a31c32eeed1c7b49e2c3
Nahuel Ambrosini <nambrosini@onapsis.com>
2020-12-19 11:08:00 -0300

    ✨ Add slack_command partial function. 🔧 Conditionally apply middleware when in production

--- covibot/app.py
+++ covibot/app.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from functools import partial
 
 from chalice import Chalice, Response
@@ -15,16 +14,17 @@ from chalicelib.db import get_database
 from chalicelib.reservas.api import reservar_semana, cancelar_reserva_semana, listar_reservas, listar_mis_reservas
 
 
-def init_app(config: Config):
+def init_app(config: Config, db=None):
     app = Chalice(app_name="covibot-api")
     app.log.setLevel(config.log_level)
 
     # Middlewares
-    app.register_middleware(validate_request_comes_from_slack, 'http')
     app.register_middleware(add_user_to_context, 'http')
-    app.register_middleware(log_all_traffic, 'all')
+    if config.production:
+        app.register_middleware(validate_request_comes_from_slack, 'http')
+        app.register_middleware(log_all_traffic, 'all')
 
-    app.db = get_database()
+    app.db = db or get_database()
     app.bus = SQSBus(queue=config.sqs_url, name=config.sqs_queue_name)
     app.slack = Slack(token=config.slack_bot_token)
 
@@ -36,17 +36,24 @@ app = init_app(config)
 JSONResponse = partial(Response, headers={'Content-Type': 'application/json'})
 Ok = partial(JSONResponse, status_code=200)
 
+# Slack slash commands are form-encoded and use the POST method
+# https://api.slack.com/interactivity/slash-commands#app_command_handling
+slack_command = partial(
+    app.route,
+    content_types=['application/x-www-form-urlencoded'],
+    methods=['POST'],
+)
 
-@app.route("/")
+
+@app.route("/", methods=['POST', 'GET'])
 def home():
     return {"success": True}
 
 
-@app.route("/reservar", methods=['POST'])
+@slack_command("/reservar")
 def reservar_handler():
     user = app.current_request.user
-    db = app.current_request.db
-
+    db = app.db
     reserva = reservar_semana(db, user)
     if reserva.ok:
         return Ok(reserva.data)
@@ -54,28 +61,25 @@ def reservar_handler():
         return Ok(reserva.data)
 
 
-@app.route("/cancelar_reserva", methods=['POST'])
-def reservar_handler():
+@slack_command("/cancelar_reserva")
+def cancelar_reserva_handler():
     user = app.current_request.user
-    db = app.current_request.db
 
-    reserva = cancelar_reserva_semana(db, user)
+    reserva = cancelar_reserva_semana(app.db, user)
     if reserva.ok:
         return Ok(reserva.data)
     else:
         return Ok(reserva.data)
 
 
-@app.route("/listar_reservas", methods=['POST'])
-def reservar_handler():
-    db = app.current_request.db
-    reservas = listar_reservas(db)
+@slack_command("/listar_reservas")
+def listar_reservas_handler():
+    reservas = listar_reservas(app.db)
     return Ok(reservas.data)
 
 
-@app.route("/mis_reservas", methods=['POST'])
+@slack_command("/mis_reservas")
 def mis_reservar_handler():
-    db = app.current_request.db
     user = app.current_request.user
-    reservas = listar_mis_reservas(db, user.username)
+    reservas = listar_mis_reservas(app.db, user.username)
     return Ok(reservas.data)
